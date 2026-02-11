fd91be5b1fae12b689b74cc89f3b8002c3469a00
avnimehtaaaaaa <mehtavni2007@gmail.com>
2026-02-11 15:40:27 +0530

    Add files via upload

---
+++ que9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int i;
+    scanf("%s", s);
+
+    for (i = strlen(s) - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
+/*
+hello
+olleh
+
+*/
